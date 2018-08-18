aa0a57496fac1a24db715469332e31560d8e19e6
Developer Walkwel <developer.walkwel@gmail.com>
2018-08-18 11:27:25 +0530

    Fixes Profile picture url for TopBar

--- resources/views/components/header/topbar.blade.php
+++ resources/views/components/header/topbar.blade.php
@@ -23,7 +23,7 @@
                             <div class="m-dropdown__header m--align-center" style="background: url("{{ asset('img/user_profile_bg.jpg') }}"); background-size: cover;">
                                 <div class="m-card-user m-card-user--skin-dark">
                                     <div class="m-card-user__pic">
-                                        <img src="{{ asset('img/user.jpg') }}" class="m--img-rounded m--marginless" alt="" />
+                                        <img src="{{ asset('img/user-1.jpg') }}" class="m--img-rounded m--marginless" alt="" />
                                     </div>
                                     <div class="m-card-user__details">
                                         <span class="m-card-user__name m--font-weight-500">
