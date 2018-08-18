b66ba15f05286745d759e4067c3cabc586750003
Developer Walkwel <developer.walkwel@gmail.com>
2018-08-18 11:27:51 +0530

    Added CSRF token to App template layout and meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <link rel="shortcut icon" href="{{ asset('favicon.ico')}}" type="image/x-icon">
 
     @include('components/required/top')
@@ -32,6 +34,9 @@
 
     @include('components/required/bottom')
 
+    <script>
+        let token = document.head.querySelector('meta[name="csrf-token"]');
+    </script>
     @stack('scripts')
 </body>
 </html>
